dc2487ac23642741cbd439536827d6120f9ae674
Oscar Eduardo Ortiz Pinzon <oscardo2000@hotmail.com>
2016-05-23 11:39:01 -0500

    version 2

---
+++ MakrosoftWeb/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MakrosoftWeb.Startup))]
+namespace MakrosoftWeb
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
