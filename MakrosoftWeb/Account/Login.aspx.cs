d511b1f4489df9c5d163c6cb062dd9103b4019d2
Oscar Eduardo Ortiz Pinzon <oscardo2000@hotmail.com>
2016-05-23 13:39:05 -0500

    version 7

---
+++ MakrosoftWeb/Account/Login.aspx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MakrosoftWeb;
+public partial class Account_Login : System.Web.UI.Page
+{
+
+    public partial class Login : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //RegisterHyperLink.NavigateUrl = "Register";
+            // Enable this once you have account confirmation enabled for password reset functionality
+            //ForgotPasswordHyperLink.NavigateUrl = "Forgot";
+            //OpenAuthLogin.ReturnUrl = Request.QueryString["ReturnUrl"];
+            var returnUrl = HttpUtility.UrlEncode(Request.QueryString["ReturnUrl"]);
+            if (!String.IsNullOrEmpty(returnUrl))
+            {
+                //RegisterHyperLink.NavigateUrl += "?ReturnUrl=" + returnUrl;
+            }
+        }
+
+        protected void LogIn(object sender, EventArgs e)
+        {
+            if (IsValid)
+            {
+                // Validate the user password
+                //U.NombreUsuario = Email.Text.ToString().ToLower();
+                //U.Password = Password.Text.ToString().ToLower();
+                CUsuarios CU = new CUsuarios();
+                var VUsuario = CU.ObtenerUnUsuario(Email.Text.ToLower().ToString(), Password.Text.ToString().ToLower());
+                if (VUsuario != null)
+                {
+                    switch (VUsuario.FKRol)
+                    {
+                        case 1:
+                            HttpCookie Cookie = CookieMacrosoft.CrearCookie(VUsuario);
+                            Response.Redirect(String.Format("/Administracion/Default.aspx"));
+                            break;
+                        default:
+                            FailureText.Text = "Error al ingresar";
+                            ErrorMessage.Visible = true;
+                            break;
+                    }
+                }
+                else
+                {
+                    FailureText.Text = "Error al ingresar";
+                    ErrorMessage.Visible = true;
+                }
+
+
+                //U.Usuario<DatosUsuario>(p => (p.NombreUsuario == User) && (p.Password == Clave));
+                //var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
+                //var signinManager = Context.GetOwinContext().GetUserManager<ApplicationSignInManager>();
+
+                // This doen't count login failures towards account lockout
+                // To enable password failures to trigger lockout, change to shouldLockout: true
+                //var result = signinManager.PasswordSignIn(Email.Text, Password.Text, RememberMe.Checked, shouldLockout: false);
+
+                //switch (result)
+                //{
+                //    case SignInStatus.Success:
+                //        IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
+                //        break;
+                //    case SignInStatus.LockedOut:
+                //        Response.Redirect("/Account/Lockout");
+                //        break;
+                //    case SignInStatus.RequiresVerification:
+                //        Response.Redirect(String.Format("/Account/TwoFactorAuthenticationSignIn?ReturnUrl={0}&RememberMe={1}", 
+                //                                        Request.QueryString["ReturnUrl"],
+                //                                        RememberMe.Checked),
+                //                          true);
+                //        break;
+                //    case SignInStatus.Failure:
+                //    default:
+                //        FailureText.Text = "Error al ingresar";
+                //        ErrorMessage.Visible = true;
+                //        break;
+                //}
+            }
+        }
+    }
+
+}
---
+++ MakrosoftWeb/App_Code/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Usuario
+/// </summary>
+public static class Usuario
+{
+        public static Usuario ObtenerUnUsuario(string User, string Clave)
+        {
+            return DatosUsuario.Usuario.SingleOrDefault<Usuario>(p => (p.NombreUsuario == User) && (p.Password == Clave));
+        }
+    
+}
